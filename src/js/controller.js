63e8d3e7ebe0263bfa38dcb64c1745768d6f0039
minotaurolol <heberestrada1997@gmail.com>
2021-06-06 01:28:52 -0500

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,10 +11,6 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
 
-// if (module.hot) {
-// 	module.hot.accept();
-// }
-
 const controlRecipes = async function () {
 	try {
 		const id = window.location.hash.slice(1);
@@ -72,7 +68,6 @@ const controlServings = function (newServings) {
 	model.updateServings(newServings);
 
 	// Update the recipe view
-	// recipeView.render(model.state.recipe);
 	recipeView.update(model.state.recipe);
 };
 
@@ -131,6 +126,5 @@ const init = function () {
 	searchView.addHandlerSearch(controlSearchResults);
 	paginationView.addHandlerClick(controlPagination);
 	addRecipeView.addHandlerUpload(controlAddRecipe);
-	console.log('Welcome!');
 };
 init();
